00877b19ab5891327cb04f9ab526c8fd0ebaa7b9
AkshadaD <akshata2696@gmail.com>
2020-06-16 14:53:48 +0530

    feat:search feature

--- src/components/screens/orderScreens/OrderList.js
+++ src/components/screens/orderScreens/OrderList.js
@@ -8,50 +8,82 @@ import {connect} from 'react-redux'
 
 
 class OrderList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orderList:'',
+        }
+    }
+    
 
     goBack = () => this.props.navigation.goBack();
 
     componentDidMount(){
         this.props.getOrderDetails('getOrderDetails');
         const {orderDetails} = this.props;
+        (orderDetails !== undefined) && this.setState({orderList:orderDetails});
         console.log("orderDetails: ", orderDetails);
     }
-    // componentDidUpdate(prevProps){
-    //     if(this.props.orderDetails !== prevProps.orderDetails){
-    //         // this.props.getOrderDetails('getOrderDetails');
-    //         const {orderDetails} = this.props;
-    //         console.log("jhhk: ",orderDetails);
-    //     }
-    // }
+
+    //to update the state with current redux state
+    componentDidUpdate(prevProps) {
+        if (this.props.orderDetails !== prevProps.orderDetails) {
+            this.setState({orderList: this.props.orderDetails});
+        }
+    }
+
+    //============
+    searchHandler = (searchText) =>{
+        const {orderDetails} = this.props;
+        const {orderList} = this.state;
+        let newData;
+
+        (orderList !== '' && searchText !== '') ?
+        (
+         newData = orderDetails.filter(item =>{
+            const itemData = `${item.product_details[0].order_id.toLowerCase()}`;
+            const searchTxt = searchText.toLowerCase();
+
+            return itemData.indexOf(searchTxt) > -1;
+        }),
+        this.setState({orderList: newData})
+        ) : 
+        (
+            this.setState({orderList: orderDetails})
+        );
+    }
+    //=============
 
     render() {
         const {orderDetails,isLoading} = this.props;
-        console.log("In render orderDetails: ", orderDetails, 'isLoading: ',isLoading);
+        // console.log("In render orderDetails: ", orderDetails, 'isLoading: ',isLoading);
+        const {orderList} = this.state;
+        console.log('Ol: ',orderList);
         
         
         return (
             <View style={{flex: 1,}}>
-                <Header iconName="arrow-left" handleLeftIconClick={this.goBack}  headerTitle="My Orders"  rightIconName="search"/>
+                <Header iconName="arrow-left" handleLeftIconClick={this.goBack}  headerTitle="My Orders"  rightIconName="search" handleRightIconClick={this.searchHandler}/>
                 
-                {(orderDetails === undefined && isLoading === true) ? 
+                {(orderDetails == undefined) ? 
                 (<ActivityIndicator size='large' />):
                 (
-                <ScrollView style={{padding: StyleConstants.PADDING}}>
+                <View style={{padding: StyleConstants.PADDING}}>
                     <FlatList
-                    data={orderDetails}
+                    data={(orderList.length <= 0 && orderDetails !== undefined)?(orderDetails):(orderList)}
                     renderItem={ ({item}) => (
                         <TouchableOpacity onPress={() => this.props.navigation.navigate('OrderDetails',{productDetails:item})}>
                             <Text style={[styles.productDetailTitle, {marginBottom:0,}]}> ID: {item.product_details[0].order_id} </Text>
                             <Text style={[ styles.productListName ,{alignSelf: 'flex-end'}]}> Rs. {item.product_details[0].total_productCost} </Text>
-                            <Text style={styles.productDetailMaterial}> Ordered Date:  </Text> 
+                            <Text style={styles.productDetailMaterial}> Ordered Date: {new Date(item.product_details[0].createdAt).toDateString()} </Text> 
                         </TouchableOpacity>
                     )}
-                    keyExtractor={(item,index) => item.product_id}
+                    keyExtractor={(item,index) => item._id}
                     ItemSeparatorComponent={() => <View style={{height: 0.5, backgroundColor:StyleConstants.COLOR_9E0100,margin: 10}}/>}
                     />
 
                     <View style={{height: 0.5, backgroundColor:StyleConstants.COLOR_9E0100,margin: 10}}/>
-                </ScrollView>
+                </View>
                 )}
             </View>
         );
