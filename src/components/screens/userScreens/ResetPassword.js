fb5c518d51ca8df9c4d4a6025005b8b9dd97b293
AkshadaD <akshata2696@gmail.com>
2020-06-16 14:55:00 +0530

    added loader and solved other issues

--- src/components/screens/userScreens/ResetPassword.js
+++ src/components/screens/userScreens/ResetPassword.js
@@ -8,6 +8,8 @@ import {StyleConstants} from '../../styles/Constants';
 import {CHANGE_PASSWORD_URLTYPE} from '../../../API/apiConstants';
 import {loggedInUserActions} from '../../../redux/actions/LoggedInUserActions';
 import {connect} from 'react-redux';
+import { customErrors } from '../../../utils/Validation';
+import Loader from '../../Common/Loader';
 
 class ResetPassword extends Component {
     constructor(props) {
@@ -17,208 +19,201 @@ class ResetPassword extends Component {
             newPass: '',
             confirmPass: '',
 
-            passIcon: 'eye',
-            passwordHide: true,
+            // passIcon: 'eye',
+            // passwordHide: true,
+            passIcon: ['eye','eye','eye'],
+            passwordHide: [true,true,true],
 
-            errors:{
-                oldPassError:'',
-                newPassError:'',
-                confirmPassError:'',
-
-            }
+            showLoader:false,
+            errors:{},
         }
     }
     
+    showLoader = () => { this.setState({ showLoader:true }); };
+    hideLoader = () => { this.setState({ showLoader:false }); };
 
     goBack = () => this.props.navigation.goBack();
 
-    setPasswordVisiblility = () => {
-        if(this.state.passIcon === 'eye'){
-            this.setState({passIcon: 'eye-slash', })
+    setPasswordVisiblility = (i) => {
+        // if(this.state.passIcon === 'eye'){
+        //     this.setState({passIcon: 'eye-slash', })
+        // }
+        // else{ 
+        //     this.setState({passIcon: 'eye', })
+        // }
+        // this.setState({passwordHide: !this.state.passwordHide });
+
+        const {passIcon,passwordHide} = this.state; 
+        if(this.state.passIcon[i] === 'eye'){
+            passIcon.splice(i,1,'eye-slash');
+            this.setState({passIcon: [...passIcon]});
         }
         else{ 
-            this.setState({passIcon: 'eye', })
-        }
-        this.setState({passwordHide: !this.state.passwordHide });
-    }
-
-    handleChange = (name, value) =>{
-        const passwordRegex = /^[a-zA-Z0-9]$/;
-        const {errors} = this.state;
-        
-        if(name == 'oldPass' && value == ''){
-            this.setState(state => {
-                return {
-                    ...state,
-                    errors: {
-                    ...state.oldPassError,
-                    oldPassError: '*Required'
-                    }
-                };
-                })
-        }
-        if(name == 'oldPass' && value !== ''){
-            this.setState(state => {
-                return {
-                    ...state,
-                    errors: {
-                    ...state.oldPassError,
-                    oldPassError: ''
-                    }
-                };
-                })
+            passIcon.splice(i,1,'eye');
+            this.setState({passIcon: [...passIcon]});
         }
 
-        if(name == 'newPass' && value == ''){
-            this.setState(state => {
-                return {
-                    ...state,
-                    errors: {
-                    ...state.newPassError,
-                    newPassError: '*Required'
-                    }
-                };
-                })
-        }
-        else if(name == 'newPass' && value != ''){
-            if(!passwordRegex.test(value)){
-                this.setState(state => {
-                    return {
-                        ...state,
-                        errors: {
-                        ...state.newPassError,
-                        newPassError: '*Password should contain characters and numbers'
-                        }
-                    };
-                }) 
-            }
-            else if(value.length < 8 || value.length > 12){
-                this.setState(state => {
-                    return {
-                        ...state,
-                        errors: {
-                        ...state.newPassError,
-                        newPassError: '*Password length should be 8 to 12 chars'
-                        }
-                    };
-                })
-            }
-            else{
-                this.setState(state => {
-                    return {
-                        ...state,
-                        errors: {
-                        ...state.newPassError,
-                        newPassError: ''
-                        }
-                    };
-                })
-            }
-    
-        }
-        
-
-        if(name == 'confirmPass' && value == ''){
-            this.setState(state => {
-                return {
-                    ...state,
-                    errors: {
-                    ...state.confirmPassError,
-                    confirmPassError: '*Required'
-                    }
-                };
-            })
-        }
-        else if(name == 'confirmPass' && value != ''){
-            if(this.state.newPass !== this.state.confirmPass){
-                this.setState(state => {
-                    return {
-                        ...state,
-                        errors: {
-                        ...state.confirmPassError,
-                        confirmPassError: '*Both password should be same'
-                        }
-                    };
-                })
-                
-            }
-            else{
-                this.setState(state => {
-                    return {
-                        ...state,
-                        errors: {
-                        ...state.confirmPassError,
-                        confirmPassError: ''
-                        }
-                    };
-                })
-            }
-        }
-       
-
-        console.log("Errors: ", errors)
+        passwordHide.splice(i,1,!this.state.passwordHide[i]);
+        this.setState({passwordHide: [...passwordHide]});
     }
     
+    handleValidation = (field_name) => {
+        const {oldPass,newPass,confirmPass} = this.state;
+        let {errors} = this.state;
+        let errorFlag = true;
+
+        //old password
+        if(field_name === 'oldPass'){
+            if(oldPass === ''){
+                errorFlag = true;
+                const {valueMissing} = customErrors.pass;
+                errors.oldPass = valueMissing
+            }
+            else{
+                errorFlag = false;
+                delete errors.oldPass;
+            }
+        }
+
+        //new password
+        if(field_name === 'newPass'){
+            if(newPass.length < 8 || newPass.length > 12){
+                errorFlag = true;
+                const {valueMissing, minLength} = customErrors.pass;
+                errors.newPass = newPass === '' ? valueMissing : minLength;
+            }
+            if(confirmPass.length !== 0 && newPass !== confirmPass){
+                errorFlag = true;
+                const {diffPassword} = customErrors.confirmPass;
+                errors.confirmPass = diffPassword;
+            }
+            else{
+                errorFlag = false;
+                delete errors.newPass;
+            }
+        }
+
+        //confirm new password
+        if(field_name === 'confirmPass'){
+            if(confirmPass.length === 0 || newPass !== confirmPass){
+                errorFlag = true;
+                const {valueMissing, diffPassword} = customErrors.confirmPass;
+                errors.confirmPass = confirmPass === '' ? valueMissing : diffPassword;
+            }
+            else{
+                errorFlag =  false;
+                delete errors.confirmPass;
+            }
+        }
+
+        this.setState({errors});
+        console.log("Error: ",errors, " errFlag: ", errorFlag);
+        return errorFlag;
+    }
 
     handleSubmit = async () =>{
+        this.showLoader();
         const data = {
             oldPass: this.state.oldPass,
             newPass: this.state.newPass,
             confirmPass: this.state.confirmPass,
         }
-        await this.props.changePassword(data, CHANGE_PASSWORD_URLTYPE);
-        const {changePasswordRes} = await this.props;
-        console.log("changePassRes in compo: ", changePasswordRes);
+        const errorFlag = this.handleValidation('oldPass') && this.handleValidation('newPass') && this.handleValidation('confirmPass');
+        console.log("EF----",errorFlag);
         
-        if(changePasswordRes){
-            (changePasswordRes.status_code === 200)&&
-            (Alert.alert(hangePasswordRes.message),  
-            this.props.navigation.navigate('EditProfile')); 
+        if(errorFlag === false){
+            await this.props.changePassword(data, CHANGE_PASSWORD_URLTYPE);
+            const {changePasswordRes} = await this.props;
+            console.log("changePassRes in compo: ", changePasswordRes);
+            
+            setTimeout(()=>{
+                this.hideLoader();
+                if(changePasswordRes !== undefined){
+                    (changePasswordRes.status_code === 200)?
+                    (
+                        Alert.alert(changePasswordRes.message),  
+                        this.props.navigation.navigate('Login')
+                    ) :
+                    (
+                        Alert.alert(changePasswordRes.message)
+                    )
+                }
+                else{
+                    Alert.alert('Something went wrong!!Please try again');
+                }
+            },3000);
+            
         }
         else{
-            Alert.alert(changePasswordRes.message);
+            this.hideLoader();
+            Alert.alert("Check all information is properly filled!!");
         }
     }
 
     render() {
-        const {passIcon, passwordHide} = this.state;
+        const {passIcon, passwordHide,errors} = this.state;
         const {oldPass, newPass, confirmPass} = this.state;
         return (
-            <ImageBackground source={require('../../../assets/images/background_img.jpeg')} style={{width: '100%', height: '100%'}}>
+            <ImageBackground source={require('../../../assets/images/background_img.jpg')} style={{width: '100%', height: '100%'}}>
                 <CustomHeader iconName="arrow-left" handleLeftIconClick={this.goBack} headerTitle="Reset Password" />
                 <ScrollView contentContainerStyle={{flexGrow:1}}>
                     <View style={styles.container}>
-                        <View style={{ flex: 1,}}>
-                            <Text style={[styles.brandName, {alignSelf: 'center',}]}>NeoSTORE</Text>
+                        <Text style={[styles.brandName, {alignSelf: 'center',}]}>NeoSTORE</Text>
 
-                            <Text style={styles.errorText}> {this.state.errors.oldPassError} </Text>
-                            <Item regular style={styles.textboxStyle}>
-                                <Icon name='lock' style={styles.textBoxIcon} size={StyleConstants.ICON_SIZE}/>
-                                <Input value={oldPass} style={styles.inputBoxText} secureTextEntry={passwordHide} onChangeText={oldPass => {{this.setState({oldPass}); this.handleChange("oldPass",this.state.oldPass)}} } onBlur={() => this.handleChange("oldPass",this.state.oldPass)} placeholder='Current Password' placeholderTextColor={StyleConstants.COLOR_RGBA_WHITE}/>
-                                <Icon active name={passIcon} style={[styles.textBoxIcon, {alignSelf:'flex-end'}]} size={StyleConstants.ICON_SIZE} onPress={this.setPasswordVisiblility}/>
-                            </Item>
+                        <Item regular style={styles.textboxStyle}>
+                            <Icon name='lock' style={styles.textBoxIcon} size={StyleConstants.ICON_SIZE}/>
+                            <Input 
+                                value={oldPass} 
+                                style={styles.inputBoxText} 
+                                secureTextEntry={passwordHide[0]} 
+                                onChangeText={oldPass => {this.setState({oldPass})}}
+                                onBlur={() => this.handleValidation("oldPass")} 
+                                placeholder='Current Password' 
+                                placeholderTextColor={StyleConstants.COLOR_RGBA_WHITE}
+                            />
+                            <Icon active name={passIcon[0]} style={[styles.textBoxIcon, {alignSelf:'flex-end'}]} size={StyleConstants.ICON_SIZE} onPress={() => this.setPasswordVisiblility(0)}/>
+                        </Item>
+                        <Text style={styles.errorText}> {errors.oldPass} </Text>
 
-                            <Text style={styles.errorText}> {this.state.errors.newPassError} </Text>
-                            <Item regular style={styles.textboxStyle}>
-                                <Icon name='lock' style={styles.textBoxIcon} size={StyleConstants.ICON_SIZE}/>
-                                <Input value={newPass} secureTextEntry={passwordHide} onChangeText={newPass => {{this.setState({newPass}); this.handleChange("newPass",this.state.newPass)}} } onBlur={() => this.handleChange("newPass",this.state.newPass)} style={styles.inputBoxText} placeholder='New Password' placeholderTextColor={StyleConstants.COLOR_RGBA_WHITE}/>
-                                <Icon active name={passIcon} style={[styles.textBoxIcon, {alignSelf:'flex-end'}]} size={StyleConstants.ICON_SIZE} onPress={this.setPasswordVisiblility}/>
-                            </Item>
+                        <Item regular style={styles.textboxStyle}>
+                            <Icon name='lock' style={styles.textBoxIcon} size={StyleConstants.ICON_SIZE}/>
+                            <Input 
+                                value={newPass} 
+                                secureTextEntry={passwordHide[1]} 
+                                onChangeText={newPass => {this.setState({newPass})} }
+                                onBlur={() => this.handleValidation("newPass")}
+                                onKeyPress={() => this.handleValidation("newPass")} 
+                                style={styles.inputBoxText} 
+                                placeholder='New Password' 
+                                placeholderTextColor={StyleConstants.COLOR_RGBA_WHITE}
+                            />
+                            <Icon active name={passIcon[1]} style={[styles.textBoxIcon, {alignSelf:'flex-end'}]} size={StyleConstants.ICON_SIZE} onPress={() => this.setPasswordVisiblility(1)}/>
+                        </Item>
+                        <Text style={styles.errorText}> {errors.newPass} </Text>
 
-                            <Text style={styles.errorText}> {this.state.errors.confirmPassError} </Text>
-                            <Item regular style={styles.textboxStyle}>
-                                <Icon name='lock' style={styles.textBoxIcon} size={StyleConstants.ICON_SIZE}/>
-                                <Input value={confirmPass} secureTextEntry={passwordHide} onChangeText={confirmPass => {{this.setState({confirmPass}); this.handleChange("confirmPass",this.state.confirmPass)}} } onBlur={() => this.handleChange("confirmPass",this.state.confirmPass)} style={styles.inputBoxText} placeholder='Confirm Password' placeholderTextColor={StyleConstants.COLOR_RGBA_WHITE}/>
-                                <Icon active name={passIcon} style={[styles.textBoxIcon, {alignSelf:'flex-end'}]} size={StyleConstants.ICON_SIZE} onPress={this.setPasswordVisiblility}/>
-                            </Item>
+                        <Item regular style={styles.textboxStyle}>
+                            <Icon name='lock' style={styles.textBoxIcon} size={StyleConstants.ICON_SIZE}/>
+                            <Input 
+                                value={confirmPass} 
+                                secureTextEntry={passwordHide[2]} 
+                                onChangeText={confirmPass => {this.setState({confirmPass})} } 
+                                onBlur={() => this.handleValidation("confirmPass")} 
+                                onKeyPress={() => this.handleValidation("confirmPass")} 
+                                style={styles.inputBoxText} 
+                                placeholder='Confirm Password' 
+                                placeholderTextColor={StyleConstants.COLOR_RGBA_WHITE}
+                            />
+                            <Icon active name={passIcon[2]} style={[styles.textBoxIcon, {alignSelf:'flex-end'}]} size={StyleConstants.ICON_SIZE} onPress={() => this.setPasswordVisiblility(2)}/>
+                        </Item>
+                        <Text style={styles.errorText}> {errors.confirmPass} </Text>
 
-                            <TouchableHighlight style={styles.button} onPress={() => this.handleSubmit()}>
-                                <Text style={styles.buttonText}> RESET PASSWORD </Text>
-                            </TouchableHighlight>
-
-                        </View>
+                        <TouchableHighlight style={styles.button} onPress={() => this.handleSubmit()}>
+                            <Text style={styles.buttonText}> RESET PASSWORD </Text>
+                        </TouchableHighlight>
 
                     </View>
+                    {(this.state.showLoader) && <Loader />}
+
                 </ScrollView>
             </ImageBackground>  
         );
